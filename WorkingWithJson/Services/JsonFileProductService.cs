a8e7a2791e6795e31d96977ed6da19d2ef94d392
Nazifullah Sahim <nsahim@my.centennialcollege.ca>
2020-01-03 19:06:08 -0500

    added toString method to Product class to convert json data to text

---
+++ WorkingWithJson/Services/JsonFileProductService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkingWithJson.Services
+{
+    public class JsonFileProductService
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; }
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+    }
+}
