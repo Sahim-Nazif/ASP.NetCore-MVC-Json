b2d1a709c50437c962e106306af0e71e8fc8157d
Nazifullah Sahim <nsahim@my.centennialcollege.ca>
2020-01-03 19:30:04 -0500

    created jsonFileProductService class with IwebhostEniv to get json data

--- WorkingWithJson/Services/JsonFileProductService.cs
+++ WorkingWithJson/Services/JsonFileProductService.cs
@@ -1,17 +1,42 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
+using WorkingWithJson.Models;
 
 namespace WorkingWithJson.Services
 {
     public class JsonFileProductService
     {
+        //the idea is that webhostenironment will identify the path from where to get our products from Json file
         public IWebHostEnvironment WebHostEnvironment { get; }
         public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
         {
             WebHostEnvironment = webHostEnvironment;
+        }
+        //getting the json file path here
+        private string JsonFileName
+        {
+            get
+            {
+                return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json");
+               
+            }
+      
+        }
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader=File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
         }
     }
 }
