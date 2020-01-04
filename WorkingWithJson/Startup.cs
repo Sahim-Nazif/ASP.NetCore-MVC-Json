e1c9bee065db81a66a6151c5efe3353ccbd81cd6
Nazifullah Sahim <nsahim@my.centennialcollege.ca>
2020-01-03 19:34:12 -0500

    added JsonFileProductService to Startup.cs

--- WorkingWithJson/Startup.cs
+++ WorkingWithJson/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WorkingWithJson.Services;
 
 namespace WorkingWithJson
 {
@@ -24,6 +25,8 @@ namespace WorkingWithJson
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddTransient<JsonFileProductService>();
 
            // Runtime compilation
            //To enable runtime compilation for all environments and configuration modes:
